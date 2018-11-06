c361da167233d3fb24cc9e03be256e680a155415
kikimetal <reezafratzitz2@gmail.com>
2018-11-06 20:41:01 +0900

    ピンタレストAPIのテスト

---
+++ pages/drawing.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="page">
+    <Logotype />
+    <router-link exact to="/">
+      Kikimetal Home
+    </router-link>
+    <router-link exact to="/drawing">
+      My drawing
+    </router-link>
+    <div id="pinterest-container">
+      <a
+      data-pin-do="embedBoard"
+      data-pin-lang="ja"
+      data-pin-board-width="1600"
+      data-pin-scale-height="1000"
+      data-pin-scale-width="10"
+      href="https://www.pinterest.jp/kikimetal/kikimetal"/>
+    </div>
+    <script async defer src="//assets.pinterest.com/js/pinit.js"></script>
+  </div>
+</template>
+
+<script>
+import Logotype from '~/components/Logotype.vue'
+
+export default {
+  components: {
+    Logotype,
+  },
+}
+</script>
+
+<style>
+#pinterest-container > span {
+  width: 100%;
+  max-width: 100%;
+  max-height: none;
+  box-shadow: none;
+  border: none;
+  border-radius: 0;
+  /* background: pink; */
+  -webkit-overflow-scrolling: touch;
+}
+</style>
